89a52b2ead0b3aba397737e12e37c79744639f8f
Diego Cortes <dcortes@campus.udes.edu.co>
2017-12-19 16:38:51 -0500

    Configuring login visual and routes

--- demo/urls.py
+++ demo/urls.py
@@ -20,5 +20,5 @@ from test_drive import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
-    url(r'^callback/$', views.login, name='login'),
+    url(r'^login/$', views.login, name='login'),
 ]
--- test_drive/views.py
+++ test_drive/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
+from django.contrib import messages
 from pprint import pprint
 
 from pydrive.auth import GoogleAuth
@@ -7,22 +8,39 @@ from pydrive.drive import GoogleDrive
 
 
 def home(request):
-    gauth = GoogleAuth()
-    gauth.DEFAULT_SETTINGS['client_config_file'] = settings.GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE
-    gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentication.
-    pprint(gauth)
-    drive = GoogleDrive(gauth)
-    file1 = drive.CreateFile({'title': 'Hello2.docx'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
-    file1.SetContentString('Hello World!')  # Set content of the file from given string.
-    file1.Upload()
-    pprint(file1)
-    return render(request, 'index.html')
+    return render(request, 'index.html', {})
 
 
 def login(request):
-    code = request.GET.get('code')
-    print("CODE -- ", code)
-    if code:
-        request.session['code'] = code
+    gauth = request.session.get('gauth')
+    if gauth:
+        messages.warning(request, "Ya se encuentra logueado.")
+        return redirect('home')
+    else:
+        gauth = GoogleAuth()
+        gauth.DEFAULT_SETTINGS[
+            'client_config_file'] = settings.GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE
+        # Creates local webserver and auto handles authentication.
+        gauth.LocalWebserverAuth()
+        pprint(gauth)
+        request.session['gauth'] = gauth
+        return redirect('home')
 
-    return redirect('home')
+
+
+def create_file(request):
+    gauth = request.session.get('gauth')
+    drive = GoogleDrive(gauth)
+    # Create GoogleDriveFile instance with title 'Hello.txt'.
+    file1 = drive.CreateFile({
+        'title': 'Hello2.docx',
+        'mimeType': 'application/vnd.google-apps.folder'
+    })
+    # Set content of the file from given string.
+    file1.SetContentString('Hello World!')
+    file1.Upload()
+    pprint(file1)
+
+
+def list_files(request):
+    pass
